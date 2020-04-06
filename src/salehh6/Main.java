e8646af77b411b6a68e3789591b4c63e259c2cdd
HeshSs <hishmat.salehi2015@gmail.com>
2020-04-05 23:37:27 -0400

    Finished Q7

--- src/salehh6/Main.java
+++ src/salehh6/Main.java
@@ -7,6 +7,8 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.KosarajuSharirSCC;
 import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.SymbolDigraph;
+import edu.princeton.cs.algs4.Topological;
 
 public class Main {
 
@@ -17,37 +19,25 @@ public class Main {
 //		Graph graph = new Graph(file);
 		
 //		// Q5 - a
-		In file = new In("tinyDGex2.txt");
+//		In file = new In("tinyDGex2.txt");
 //		Digraph graph = new Digraph(file);
 				
 		// Q6
-//		In file = new In("Q6.txt");
+		In file = new In("Q6.txt");
 		Digraph G = new Digraph(file);
 		
-		System.out.println(G);
+//		System.out.println(G);
 
-		KosarajuSharirSCC scc = new KosarajuSharirSCC(G);
-		
-        // number of connected components
-        int m = scc.count();
-        System.out.println(m + " strong components");
-
-        // compute list of vertices in each strong component
-        Queue<Integer>[] components = (Queue<Integer>[]) new Queue[m];
-        for (int i = 0; i < m; i++) {
-            components[i] = new Queue<Integer>();
-        }
-        for (int v = 0; v < G.V(); v++) {
-            components[scc.id(v)].enqueue(v);
-        }
-
-        // print results
-        for (int i = 0; i < m; i++) {
-            for (int v : components[i]) {
-                StdOut.print(v + " ");
-            }
-            StdOut.println();
-        }				
+//		KosarajuSharirSCC scc = new KosarajuSharirSCC(G);
+						
+		// Q7
+        String filename  = "Q7.txt";
+        String delimiter = "/";
+        SymbolDigraph sg = new SymbolDigraph(filename, delimiter);
+        Topological topological = new Topological(sg.digraph());
+        for (int v : topological.order()) {
+            StdOut.println(sg.nameOf(v));
+        }		
 	}
 
 }
