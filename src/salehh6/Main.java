0cfbad13c6352fbe6b58e727dfe4652d9f53656e
HeshSs <hishmat.salehi2015@gmail.com>
2020-04-07 13:28:55 -0400

    Finished Q17

--- src/salehh6/Main.java
+++ src/salehh6/Main.java
@@ -2,6 +2,7 @@ package salehh6;
 
 import edu.princeton.cs.algs4.AcyclicSP;
 import edu.princeton.cs.algs4.BellmanFordSP;
+import edu.princeton.cs.algs4.BoyerMoore;
 import edu.princeton.cs.algs4.DijkstraSP;
 import edu.princeton.cs.algs4.DirectedEdge;
 import edu.princeton.cs.algs4.EdgeWeightedDigraph;
@@ -25,7 +26,23 @@ public class Main {
 //		In file = new In("Q6.txt");
 //		Digraph G = new Digraph(file);
 
+        String pat = "a b a c a b";
+        String txt = "b a c a a b a c c a b a c a b a a b b";
+        char[] pattern = pat.toCharArray();
+        char[] text    = txt.toCharArray();
 
+        BoyerMoore boyermoore1 = new BoyerMoore(pat);
+        BoyerMoore boyermoore2 = new BoyerMoore(pattern, 256);
+        int offset1 = boyermoore1.search(txt);
+        int offset2 = boyermoore2.search(text);
+
+        // print results
+        StdOut.println("text:    " + txt);
+
+        StdOut.print("pattern: ");
+        for (int i = 0; i < offset1; i++)
+            StdOut.print(" ");
+        StdOut.println(pat);
 
 	}
 }
