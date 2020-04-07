1631eac442fb6de5d6e8286a351a2089c7ec9e28
HeshSs <hishmat.salehi2015@gmail.com>
2020-04-07 15:02:15 -0400

    Finished the template for all questions.

--- src/salehh6/Main.java
+++ src/salehh6/Main.java
@@ -8,6 +8,7 @@ import edu.princeton.cs.algs4.DirectedEdge;
 import edu.princeton.cs.algs4.EdgeWeightedDigraph;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.KMP;
+import edu.princeton.cs.algs4.RabinKarp;
 import edu.princeton.cs.algs4.StdOut;
 
 public class Main {
@@ -28,19 +29,16 @@ public class Main {
 
         String pat = "a b a c a b";
         String txt = "b a c a a b a c c a b a c a b a a b b";
-        char[] pattern = pat.toCharArray();
-        char[] text    = txt.toCharArray();
 
-        BoyerMoore boyermoore1 = new BoyerMoore(pat);
-        BoyerMoore boyermoore2 = new BoyerMoore(pattern, 256);
-        int offset1 = boyermoore1.search(txt);
-        int offset2 = boyermoore2.search(text);
+        RabinKarp searcher = new RabinKarp(pat);
+        int offset = searcher.search(txt);
 
         // print results
         StdOut.println("text:    " + txt);
 
+        // from brute force search method 1
         StdOut.print("pattern: ");
-        for (int i = 0; i < offset1; i++)
+        for (int i = 0; i < offset; i++)
             StdOut.print(" ");
         StdOut.println(pat);
 
