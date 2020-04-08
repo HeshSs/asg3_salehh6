73e0720d4cb10a45073d9ed2902724b37cbd0dc1
HeshSs <hishmat.salehi2015@gmail.com>
2020-04-07 20:20:58 -0400

    Finished A3

--- src/salehh6/Main.java
+++ src/salehh6/Main.java
@@ -1,16 +1,5 @@
 package salehh6;
 
-import edu.princeton.cs.algs4.AcyclicSP;
-import edu.princeton.cs.algs4.BellmanFordSP;
-import edu.princeton.cs.algs4.BoyerMoore;
-import edu.princeton.cs.algs4.DijkstraSP;
-import edu.princeton.cs.algs4.DirectedEdge;
-import edu.princeton.cs.algs4.EdgeWeightedDigraph;
-import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.KMP;
-import edu.princeton.cs.algs4.RabinKarp;
-import edu.princeton.cs.algs4.StdOut;
-
 public class Main {
 
 	public static void main(String[] args) {
@@ -26,21 +15,6 @@ public class Main {
 		// Q6
 //		In file = new In("Q6.txt");
 //		Digraph G = new Digraph(file);
-
-        String pat = "a b a c a b";
-        String txt = "b a c a a b a c c a b a c a b a a b b";
-
-        RabinKarp searcher = new RabinKarp(pat);
-        int offset = searcher.search(txt);
-
-        // print results
-        StdOut.println("text:    " + txt);
-
-        // from brute force search method 1
-        StdOut.print("pattern: ");
-        for (int i = 0; i < offset; i++)
-            StdOut.print(" ");
-        StdOut.println(pat);
 
 	}
 }
